6a765b9e53363029e64200ea5598be879af7f2c6
enes4u <enesuzunyearup@gmail.com>
2025-04-15 13:57:48 -0400

    Exercise 4 --TheaterReservations

---
+++ TheaterReservations/src/main/java/ReservationApp.java
@@ -0,0 +1,45 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class ReservationApp {
+
+    public static void main(String[] args) {
+        Scanner scheduler = new Scanner(System.in);
+
+        //full name
+        System.out.println("Please enter your name: ");
+        String fullName = scheduler.nextLine().trim();
+
+        //prompt the user for the date of the show
+        System.out.println("What date will you be coming (MM/dd/yyyy): ");
+        String date = scheduler.nextLine().trim();
+        DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+        LocalDate formatted = LocalDate.parse(date, formatDate);
+
+
+
+        //how many tickets
+        System.out.println("How many tickets would you like? ");
+        int numTickets =scheduler.nextInt();
+
+        //parse name -- first last
+        String[] nameParts = fullName.split("\\s+");
+        String firstName = nameParts[0];
+        String lastName = nameParts[1];
+
+        //confirmation message
+        String ticketWord = (numTickets==1) ? "ticket" : "tickets" ;
+
+        System.out.printf("%d %s reserved for %s under %s, %s%n", numTickets, ticketWord, date, lastName, firstName);
+       System.out.println(formatted.toString());
+        scheduler.close();
+
+
+
+
+
+    }
+}
