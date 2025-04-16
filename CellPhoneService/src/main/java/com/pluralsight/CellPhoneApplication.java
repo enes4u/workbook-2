154317de4b1ab33958647f9038e089466eff87dc
enes4u <enesuzunyearup@gmail.com>
2025-04-16 18:35:45 -0400

    getters and setters working fine now

--- CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplication.java
+++ CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -12,16 +12,26 @@ public class CellPhoneApplication {
         myPhone.setSerialNumber(serialNumber);
         System.out.println(serialNumber);// left here
 
+        System.out.println("Who is the carrier?");
         String carrier = askQs.nextLine();
         myPhone.setCarrier(carrier);
+        System.out.println(carrier);
+
+
         System.out.println("What model is the phone?");
         String  model = askQs.nextLine();
+        myPhone.setModel(model);
+        System.out.println(model);
 
         System.out.println("What is the phone number?");
         String  phoneNumber = askQs.nextLine();
+        myPhone.setPhoneNumber(phoneNumber);
+        System.out.println(phoneNumber);
 
         System.out.println("Who is the owner of the phone?");
         String  owner = askQs.nextLine();
+        myPhone.setOwner(owner);
+        System.out.println(owner);
 
 
     }
